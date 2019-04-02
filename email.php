0c1b9bbff137c63aff40f4a4a85b13dc402d8f5a
carolineal <45881804+carolineal@users.noreply.github.com>
2019-04-02 16:36:12 -0300

    Add files via upload

---
+++ email.php
@@ -0,0 +1,37 @@
+<?php
+
+
+if(isset($_POST['email'])) && !empty($_POST['email']){
+
+$nome = addslashes($_POST['name'])
+$email = addslashes($_POST['email'])
+$message = addslashes($_POST['message'])
+
+
+$to = "admkr1@hotmail.com";
+$subjet = "contato - site";
+$body = "nome: ".$nome. "\n"
+		"email: ".$email. "\n"
+		"message: ".$message
+
+$header = "from:caroline.oliveira2011@hotmail.com"."\r\n".Replay-To:".$email."\e\n"
+	.X=Mailer:PHP/".phpversion();
+
+if(mail($t,$subjet,$body,$header)){
+
+	echo("Email enviado com sucesso!")
+
+}else{
+	echo("O email não pode ser enviado, tente novamente.")
+}
+
+
+}
+
+
+
+
+
+}
+
+?>
